9f74e65aff1b1e168e312bc6de9d92fbbef0f85a
aMinjon1065 <59558450+aminjon1065@users.noreply.github.com>
2022-06-02 11:08:04 +0500

    0.0.1

--- src/components/NavMenu/index.jsx
+++ src/components/NavMenu/index.jsx
@@ -3,30 +3,31 @@ import {Nav, Button, Image} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 import inbox from './../../assets/icons/inbox.svg'
 import send from './../../assets/icons/send.svg'
+import draft from './../../assets/icons/draft.svg'
 const Index = () => {
     return (
         <>
             <Nav defaultActiveKey="/home" className="flex-column">
-                <Button variant="" className="text-start hover-btn rounded-3 py-2 ps-2" as={Link} to="/inbox">
+                <Button variant="" className="text-start hover-btn rounded-3 py-2 ps-2 shadow-none" as={Link} to="/dashboard/inbox">
                     <span className="pe-2">
                        {/*<AiOutlineInbox className="fs-5"/>*/}
                         <Image src={inbox} width={24}/>
                     </span>
-                    Inbox
+                    Входящие
                 </Button>
-                <Button variant="" className="text-start hover-btn rounded-3 py-2 ps-2" as={Link} to="/inbox">
+                <Button variant="" className="text-start hover-btn rounded-3 py-2 ps-2 shadow-none" as={Link} to="/dashboard/sent">
                     <span className="pe-2">
                        {/*<AiOutlineInbox className="fs-5"/>*/}
                         <Image src={send} width={24}/>
                     </span>
-                    Send
+                    Отправленные
                 </Button>
-                <Button variant="" className="text-start hover-btn rounded-3 py-2 ps-2" as={Link} to="/inbox">
+                <Button variant="" className="text-start hover-btn rounded-3 py-2 ps-2 shadow-none" as={Link} to="/dashboard/drafts">
                     <span className="pe-2">
                        {/*<AiOutlineInbox className="fs-5"/>*/}
-                        <Image src={inbox} width={24}/>
+                        <Image src={draft} width={24}/>
                     </span>
-                    Inbox
+                    Черновики
                 </Button>
             </Nav>
         </>
--- src/routes/DashboardRoutes/index.jsx
+++ src/routes/DashboardRoutes/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Inbox from "../../views/Dashboard/Mails/Inbox";
+import Sent from "../../views/Dashboard/Mails/Sent";
+import Drafts from "../../views/Dashboard/Mails/Drafts";
 import Main from "../../views/Dashboard/Main";
 import NotFound from "./../../views/NotFound/";
 const Index = () => {
@@ -8,6 +10,8 @@ const Index = () => {
     <Routes>
       <Route path={"/"} element={<Main />} />
       <Route path={"/inbox"} element={<Inbox />} />
+      <Route path={"/sent"} element={<Sent />} />
+      <Route path={"/drafts"} element={<Drafts />} />
       <Route path={"*"} element={<NotFound />} />
     </Routes>
   );
